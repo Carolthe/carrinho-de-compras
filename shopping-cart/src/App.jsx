f0923e03ba07df0dd589cfd18709c58c87aff266
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-12 22:23:44 +0100

    shopping-cart

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,20 +1,21 @@
 import './App.css'
 import Produto from './components/Produto'
-import CremeBruleeMobile from '..Img/CremeBruleeMobile.png'
+// import CremeBruleeMobile from '.Img/CremeBruleeMobile.png'
 
 function App() {
 
   return (
 <div>
 <h1 className='text-4xl mx-7 mt-10 mb-10 font-bold'>Desserts</h1>
-<Produto  src={CremeBruleeMobile}/>
+<Produto  />
+<Produto   />
+<Produto/>
+<Produto/>
+<Produto/>
+<Produto/>
+<Produto/>
+<Produto/>
 <Produto />
-<Produto/>
-<Produto/>
-<Produto/>
-<Produto/>
-<Produto/>
-<Produto/>
 </div>
   )
 }
--- shopping-cart/src/components/Produto.jsx
+++ shopping-cart/src/components/Produto.jsx
@@ -2,8 +2,8 @@ import WaffleMobile from '../Img/WaffleMobile.png'
 
 export default function Produto (){
     return(
-        <div className='mx-7 mb-10'>
-        <div className='grid justify-center justify-items-center '>
+        <div className='mx-7 mb-10 '>
+        <div className='grid justify-center justify-items-center sm:'>
             <img src={WaffleMobile} className='w-[340px] h-52 rounded-xl ' />
             <div className='flex relative bottom-7  '>
             <button className='bg-white w-48 h-14 rounded-full border-amber-800	border-2 text-lg font-semibold '>Add to Cart</button>
@@ -11,8 +11,8 @@ export default function Produto (){
         </div>
         <div>
             <h4 className='text-xl font-semibold text-opacity-70 text-orange-950'> Waffle</h4>
-            <p className='text-2xl font-bold'>Waffle with Berries</p>
-            <p className='text-2xl font-semibold text-orange-700'>$6.50</p>
+            <p className='text-xl font-bold'>Waffle with Berries</p>
+            <p className='text-xl font-semibold text-orange-700'>$6.50</p>
         </div>
 </div>
     )
