d5d96ed59cd90bf5d721572765b88c4026ab03ef
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-13 15:35:52 +0100

    shopping-cart

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,21 +1,30 @@
 import './App.css'
 import Produto from './components/Produto'
-// import CremeBruleeMobile from '.Img/CremeBruleeMobile.png'
+import WaffleMobile from './Img/WaffleMobile.png'
+import CremeBruleeMobile from './Img/CremeBruleeMobile.png'
+import MacaronMobile from './Img/MacaronMobile.png'
+import TiramisuMobile from './Img/TiramisuMobile.png'
+import BaklavaMobile from './Img/BaklavaMobile.png'
+import CakeMobile from './Img/CakeMobile.png'
+import BrownieMobile from './Img/BrownieMobile.png'
+import PannaCottaMobile from './Img/PannaCottaMobile.png'
+import MeringueMobile from './Img/MeringueMobile.png'
+
 
 function App() {
 
   return (
 <div>
 <h1 className='text-4xl mx-7 mt-10 mb-10 font-bold'>Desserts</h1>
-<Produto sobremesa="Waffle" especificação="Waffle with Berries" preço="$6.50"/>
-<Produto  sobremesa="Crème Brûlée" especificação="Vanilla Bean Crème Brûlée" preço="$7.00" />
-<Produto sobremesa="Macaron" especificação="Macaron Mix of Five" preço="$8.00" />
-<Produto sobremesa="Tiramisu" especificação="Classic Tiramisu" preço="$5.50" />
-<Produto sobremesa="Baklava"  especificação="Pistachio Baklava" preço="$4.00" />
-<Produto sobremesa="Pie" especificação="Lemon Meringue Pie" preço="$5.00" />
-<Produto sobremesa="Cake" especificação="Red Velvet Cake" preço="$4.50" />
-<Produto sobremesa="Brownie" especificação="Salted Caramel Brownie" preço="$5.50" />
-<Produto sobremesa="Panna Cotta" especificação="Vanilla Panna Cotta" preço="$6.50" />
+<Produto sobremesa="Waffle" especificação="Waffle with Berries" preço="$6.50" img={WaffleMobile}/>
+<Produto  sobremesa="Crème Brûlée" especificação="Vanilla Bean Crème Brûlée" preço="$7.00" img={CremeBruleeMobile} />
+<Produto sobremesa="Macaron" especificação="Macaron Mix of Five" preço="$8.00" img={MacaronMobile} />
+<Produto sobremesa="Tiramisu" especificação="Classic Tiramisu" preço="$5.50" img={TiramisuMobile} />
+<Produto sobremesa="Baklava"  especificação="Pistachio Baklava" preço="$4.00" img={BaklavaMobile} />
+<Produto sobremesa="Pie" especificação="Lemon Meringue Pie" preço="$5.00" img={MeringueMobile} />
+<Produto sobremesa="Cake" especificação="Red Velvet Cake" preço="$4.50" img={CakeMobile} />
+<Produto sobremesa="Brownie" especificação="Salted Caramel Brownie" preço="$5.50" img={BrownieMobile} />
+<Produto sobremesa="Panna Cotta" especificação="Vanilla Panna Cotta" preço="$6.50" img={PannaCottaMobile} />
 </div>
   )
 }
--- shopping-cart/src/components/Produto.jsx
+++ shopping-cart/src/components/Produto.jsx
@@ -1,11 +1,11 @@
-import WaffleMobile from '../Img/WaffleMobile.png'
+
 import ShoppingCart from '../Img/ShoppingCart.png'
 
 export default function Produto (props){
     return(
         <div className='mx-7 mb-10 '>
         <div className='grid justify-center justify-items-center sm:'>
-            <img src={WaffleMobile} className='w-[340px] h-52 rounded-xl ' />
+            <img src={props.img} className='w-[340px] h-52 rounded-xl ' />
             <div className='flex relative bottom-7  '>
             <button className='bg-white w-48 flex justify-center items-center h-14 rounded-full border-amber-800	border-2 text-lg font-semibold '>Add to Cart <img className='ml-1' src={ShoppingCart} /> </button>
         </div>
